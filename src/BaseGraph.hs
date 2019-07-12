339249003fed1484b0ce7e96462785989e72c33e
Sanela Numanovic <sanela90@gmail.com>
2019-07-12 14:52:17 +0200

    connections in graph

--- src/BaseGraph.hs
+++ src/BaseGraph.hs
@@ -11,7 +11,7 @@ module BaseGraph (
     -- graph update
      addEdge,
      removeEdge,
---     addVertex,
+     addVertex,
 --     removeVertex,
 
     -- graph visualization
@@ -24,9 +24,13 @@ module BaseGraph (
     getVertexCount,
     getAllPaths,
     getPaths,
+    findFirstNeighbors,
+
 --     getDeg,
 --
 --     -- graph algorithms
+    depthFirst,
+    getConnectedComponents
 --     dfs,
 --     bfs
 
@@ -35,6 +39,7 @@ module BaseGraph (
 
 import Data.Array
 import Data.List
+import Control.Monad (guard)
 
 type Vertex = Int
 
@@ -78,9 +83,48 @@ removeEdge :: Edge -> BaseGraph -> BaseGraph
 removeEdge _edge _graph = createBaseGraph (getVertexInterval _graph) (removeElem _edge (getEdges _graph))
 
 removeElem _ [] = []
-removeElem x (y:ys) | x == y    = removeElem x ys
+removeElem x (y:ys) | x == y = removeElem x ys
                     | otherwise = y : removeElem x ys
 
+incrementVertexInterval :: BaseGraph -> Edge
+incrementVertexInterval _graph = (minimum (getVertices _graph), maximum (getVertices _graph) + 1)
+
+addVertex :: BaseGraph -> BaseGraph
+addVertex _graph = createBaseGraph (incrementVertexInterval _graph) (getEdges _graph)
+
+
+findFirstNeighbors :: BaseGraph -> Vertex -> [Vertex]
+findFirstNeighbors _graph _vertex = snd $ unzip $ filter (\(u,_) -> u == _vertex) (getEdges _graph)
+
+depthFirst :: BaseGraph -> Vertex -> [Vertex]
+depthFirst _graph _vertex = depthFirst' (getVertices _graph, getEdges _graph) _vertex
+
+depthFirst' :: ([Vertex], [Edge]) -> Vertex -> [Vertex]
+depthFirst' (_vertices, _edges) _vertex
+    | [ x | x <- _vertices, x == _vertex] == [] = []
+    | otherwise = dfRecursive (_vertices, _edges) [_vertex]
+
+dfRecursive :: ([Vertex], [Edge]) -> [Vertex] -> [Vertex]
+dfRecursive ([],_) _ = []
+dfRecursive (_,_) [] = []
+dfRecursive (_vertices, _edges) (top:_stack)
+    | [x | x <- _vertices, x == top] == [] = dfRecursive (newv, _edges) _stack
+    | otherwise = top : dfRecursive (newv, _edges) (adjacent ++ _stack)
+    where
+        adjacent = [x | (x,y) <- _edges, y == top] ++ [x | (y,x) <-_edges, y == top]
+        newv = [x | x <- _vertices, x /= top]
+
+getConnectedComponents :: BaseGraph -> [[Vertex]]
+getConnectedComponents _graph = getConnectedComponents' (getVertices _graph, getEdges _graph)
+
+getConnectedComponents' :: ([Vertex], [Edge]) -> [[Vertex]]
+getConnectedComponents' ([],_) = []
+getConnectedComponents' (top:_vertices, _edges)
+    | remaining == [] = [connected]
+    | otherwise = connected : getConnectedComponents' (remaining, _edges)
+    where
+        connected = depthFirst' (top:_vertices, _edges) top
+        remaining = (top:_vertices) \\ connected
 
 
 
